e242e6adfb15888a5667dc82d189ccabc10380a7
kattroll <katjah@hotmail.com>
2020-08-20 13:29:37 +0300

    Updates

---
+++ precache-manifest.8967df13e7561b17f65b96aef9b66e17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4320fc5667cb0eb7e21f631fa606fb6a",
+    "url": "/personaltrainer/index.html"
+  },
+  {
+    "revision": "19384dd8618b2e4457e8",
+    "url": "/personaltrainer/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6ed2c87f0a97ed2ad924",
+    "url": "/personaltrainer/static/js/2.50a5ab05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personaltrainer/static/js/2.50a5ab05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19384dd8618b2e4457e8",
+    "url": "/personaltrainer/static/js/main.2f6f0ebb.chunk.js"
+  },
+  {
+    "revision": "1d0158cbe067a626a525",
+    "url": "/personaltrainer/static/js/runtime-main.b0b2c887.js"
+  }
+]);
